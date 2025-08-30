4343ff53cf12b858a08a0995b448d2634345827d
Aria Corona <jcorona.lux@gmail.com>
2025-08-30 17:27:14 -0400

    Create script to upload backup files to google cloud storage and delete from the backup folder.

---
+++ src/scripts/upload_backups.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from src.util.gcloud_helper import GCloudBucket
+import os
+
+BACKUP_PATH = "/user/alice/Backups_Local/"
+BUCKET_NAME = "alice-api-backups"
+
+files = [
+    file
+    for file in os.listdir(BACKUP_PATH)
+    if os.path.isfile(os.path.join(BACKUP_PATH, file))
+]
+
+gcloud_bucket = GCloudBucket(bucket_name=BUCKET_NAME)
+
+for file in files:
+    destination_blob = f"backups/{os.path.basename(file)}"
+    file_path = os.path.join(BACKUP_PATH, file)
+    try:
+        gcloud_bucket.upload_to_bucket(file_path, destination_blob)
+        print(f"Successfully uploaded {file_path} to {destination_blob}")
+        os.remove(file_path)
+    except Exception as e:
+        print(f"Failed to upload {file_path} to {destination_blob}: {e}", exc_info=True)
+
+print(f"Backup upload process completed.")
