ef5e8927960293af9d96f6626d9db1920680fc26
Aria Corona <corona.aria.a@gmail.com>
2025-09-13 17:39:31 -0400

    Fix record parsing for SQL entry.

--- src/scripts/map_osrs_items.py
+++ src/scripts/map_osrs_items.py
@@ -42,7 +42,8 @@ def create_map_columns(records: list[dict]) -> dict:
         for key, value in record.items():
             if key not in recorded_keys and value:
                 col_type = guess_column_type(value)
-                columns.append((key, col_type))
+                column_kit = {"name": key, "type": col_type}
+                columns.append(column_kit)
                 recorded_keys.add(key)
     log.info(f"Found columns from records: {columns}")
     return columns
@@ -51,8 +52,8 @@ def create_map_columns(records: list[dict]) -> dict:
 def validate_table(conn, cursor, columns: dict):
     log.info("Validating table structure.")
     validated = ensure_table_exists(
-        conn=conn,
         cursor=cursor,
+        connection=conn,
         database=DB_NAME,
         log=log,
         schema_name=SCHEMA_NAME,
@@ -67,26 +68,35 @@ def validate_table(conn, cursor, columns: dict):
 
 
 def add_records(conn, cursor, records: list[dict]):
+    def _parse_record(_record: list[dict]) -> tuple[list, list]:
+        _values = []
+        _columns = []
+        for _key, _value in _record.items():
+            _columns.append(_key)
+            _values.append(_value)
+        return _values, _columns  
+    
     log.info("Adding or updating records in the database.")
     for record in records:
+        values, columns = _parse_record(record)
         add_update_record(
-            conn=conn,
             cursor=cursor,
-            schema_name=SCHEMA_NAME,
-            table_name=TABLE_NAME,
-            record=record,
-            pk=PK,
-            log=log,
+            connection=conn,
+            database=DB_NAME,
+            schema=SCHEMA_NAME,
+            table=TABLE_NAME,
+            columns=columns,
+            values=values,
+            conflict_target=PK,
         )
     log.info("All records processed.")
 
 
 def main():
     log.info("Starting OSRS item mapping process.")
+    conn = init_psql_connection(db=DB_NAME)
+    cursor = create_cursor(connection=conn)
     try:
-        conn = init_psql_connection(db_name=DB_NAME, log=log)
-        cursor = create_cursor(conn=conn, log=log)
-
         records = get_map_data()
         if not records:
             log.error("No mapping data retrieved from WikiDataGetter.")
