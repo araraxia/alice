d6127888fb6dee6fde79a990684108da614ce4cc
Aria Corona <corona.aria.a@gmail.com>
2025-09-15 16:30:48 -0400

    Refactor to handle receiving arguments to change what price type to retrieve and update

--- src/scripts/get_osrs_item_prices.py
+++ src/scripts/get_osrs_item_prices.py
@@ -19,6 +19,7 @@ from src.util.sql_helper import (
 from src.osrs.get_item_data import WikiDataGetter
 from functools import wraps
 from datetime import datetime
+import psycopg2.errors as psql_errors
 
 log = Logger(
     log_name="PriceOSRSItems",
@@ -53,18 +54,18 @@ def init_connection(func):
     return wrapper
 
 
-def create_data_columns(records: dict) -> dict:
+def create_data_columns(records: dict) -> list[dict]:
     """
     Creates data columns from the provided records by inferring the column types.
     Args:
-        records (list[dict]): List of records to analyze for column creation.
+        records (dict): Dictionary of records to analyze for column creation.
     Returns:
-        dict: A dictionary with column names as keys and their inferred types as values.
+        list[dict]: A dictionary with column names as keys and their inferred types as values.
     """
     log.info("Creating data columns from records.")
     if not records:
         log.error("No records provided to create_data_columns.")
-        return {}
+        return [{}]
 
     columns = [{"name": PK, "type": "TIMESTAMP"}]
     recorded_keys = [PK]
@@ -81,12 +82,13 @@ def create_data_columns(records: dict) -> dict:
 
 def validate_tables(
     conn, cursor, table_name_mod: str, columns: dict, records: dict
-) -> bool:
+) -> tuple[list, list]:
     validated_tables = []
     invalid_tables = []
+    table_name_list = [str(record_id)+table_name_mod for record_id in records.keys()]
+    log.info(f"Validating table structure for {table_name_list}.")
     for record_id, record in records.items():
         table_name = str(record_id) + table_name_mod
-        log.info(f"Validating table structure for {table_name}.")
         validated = ensure_table_exists(
             cursor=cursor,
             connection=conn,
@@ -120,6 +122,71 @@ def validate_tables(
     return validated_tables, invalid_tables
 
 
+def update_records(conn, cursor, records: list[dict], table_name: str, **kwargs):
+    name_mod = kwargs.get("name_mod", "")
+    for record in records:
+        now = datetime.now()
+        column_names, column_values = [PK], [now]
+        for key, value in record.items():
+            column_names.append(str(key))
+            column_values.append(value)
+        if not column_names or not column_values:
+            continue
+        if len(column_names) != len(column_values):
+            log.error(
+                "Column names and values not equal length for table: " + table_name
+            )
+            continue
+        try:
+            add_update_record(
+                cursor=cursor,
+                connection=conn,
+                database=DB_NAME,
+                schema=SCHEMA_NAME,
+                table=table_name,
+                columns=column_names,
+                values=column_values,
+                conflict_target=PK,
+            )
+        except psql_errors.UndefinedTable as e:
+            if not kwargs.get("retry", True):
+                log.error(f"Table {table_name} does not exist and retry disabled: {e}")
+                continue
+            log.warning(f"Table {table_name} does not exist: {e}")
+            retry_update_record(conn, cursor, record, table_name, name_mod)
+        except psql_errors.UndefinedColumn as e:
+            if not kwargs.get("retry", True):
+                log.error(f"Column does not exist in {table_name}: {e}")
+                continue
+            log.warning(f"Column does not exist in {table_name}: {e}")
+            retry_update_record(conn, cursor, record, table_name, name_mod)
+        except Exception as e:
+            log.error(f"Failed to add/update record in {table_name}: {e}")
+            continue
+
+
+def retry_update_record(conn, cursor, record, table_name, name_mod):
+    item_id = table_name.rsplit("_", 1)
+    temp_record = {item_id: record}
+    columns = create_data_columns(temp_record)
+    if not columns:
+        log.error(f"Failed to create columns for {item_id}.")
+        return
+    
+    valid_tables, failed_tables = validate_tables(
+        conn=conn,
+        cursor=cursor,
+        table_name_mod=name_mod,
+        columns=columns,
+        records=temp_record,
+    )
+    if failed_tables:
+        log.error(f"Failed to create table for {item_id}: {failed_tables}")
+        return
+    log.info(f"Retrying to add record for {item_id} after creating table.")
+    update_records(conn, cursor, [record], table_name, retry=False)
+
+
 def get_latest_prices() -> list[dict]:
     return wiki_getter.get_data(endpoint="latest_prices")
 
@@ -133,63 +200,110 @@ def get_1hr_prices() -> list[dict]:
 
 
 @init_connection
-def update_latest_prices(*args, **kwargs):
+def update_prices(*args, **kwargs):
     conn = args[0]
     cursor = args[1]
     validate = kwargs.get("validate", True)
-    response = get_latest_prices()
-    records = response.get("data", {})
-    log.info("Updating latest prices.")
-    if not records:
-        log.warning("No records to update for latest prices.")
-        return
 
-    now = datetime.now()
+    get_map = {
+        "update_latest_prices": get_latest_prices,
+        "update_5min_prices": get_5min_prices,
+        "update_1hr_prices": get_1hr_prices,
+    }
 
-    if validate:
-        columns = create_data_columns(records)
-        if not columns:
-            log.error("Failed to create columns for latest prices.")
-            return
+    table_mod_map = {
+        "update_latest_prices": "_latest",
+        "update_5min_prices": "_5min",
+        "update_1hr_prices": "_1hr",
+    }
 
-        valid_tables, failed_tables = validate_tables(
-            conn=conn,
-            cursor=cursor,
-            table_name_mod="_latest",
-            columns=columns,
-            records=records,
-        )
-        if failed_tables:
-            log.error(f"Failed to validate tables: {failed_tables}")
-            return
-
-    for item_id, record in records.items():
-        column_names, column_values = [PK], [now]
-        for key, value in record.items():
-            column_names.append(str(key))
-            column_values.append(value)
-        if not column_names or not column_values:
+    for update_key, update_func in get_map.items():
+        if not kwargs.get(update_key, False):
             continue
-        if len(column_names) != len(column_values):
-            log.error(
-                "Column names and values not equal length for table: " + str(item_id)
+        response = update_func()
+        records = response.get("data", {})
+        log.info(f"Updating {update_key}.")
+        if not records:
+            log.warning(f"No records to update for {update_key}.")
+            continue
+
+        # Validate tables if needed
+        if validate:
+            columns = create_data_columns(records)
+            if not columns:
+                log.error(f"Failed to create columns for {update_key}.")
+                continue
+
+            valid_tables, failed_tables = validate_tables(
+                conn=conn,
+                cursor=cursor,
+                table_name_mod=table_mod_map[update_key],
+                columns=columns,
+                records=records,
             )
-        add_update_record(
-            cursor=cursor,
-            connection=conn,
-            database=DB_NAME,
-            schema=SCHEMA_NAME,
-            table=f"{str(item_id)}_latest",
-            columns=column_names,
-            values=column_values,
-            conflict_target=PK,
-        )
-    log.info("Latest prices updated successfully.")
+            if failed_tables:
+                log.error(f"Failed to validate tables: {failed_tables}")
+                continue
+
+        for item_id, record in records.items():
+            table_name = str(item_id) + table_mod_map[update_key]
+            try:
+                update_records(
+                    conn=conn,
+                    cursor=cursor,
+                    records=[record],
+                    table_name=table_name,
+                    name_mod=table_mod_map[update_key],
+                )
+            except Exception as e:
+                log.error(f"Failed to update {update_key} for {item_id}: {e}")
+                continue
+        log.info(f"{update_key} updated successfully.")
+
+
+def argsparser():
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="Update OSRS item prices in the database."
+    )
+    parser.add_argument(
+        "--validate",
+        action="store_true",
+        help="Do table validation before updating prices.",
+        default=True,
+    )
+    parser.add_argument(
+        "--update-latest-prices",
+        action="store_true",
+        help="Update latest prices.",
+        default=False,
+    )
+    parser.add_argument(
+        "--update-5min-prices",
+        action="store_true",
+        help="Update 5 minute prices.",
+        default=False,
+    )
+    parser.add_argument(
+        "--update-1hr-prices",
+        action="store_true",
+        help="Update 1 hour prices.",
+        default=False,
+    )
+
+    return parser.parse_args()
 
 
 def main():
+    args = argsparser()
     log.info("Starting OSRS item price update script.")
-    update_latest_prices(validate=False)
+    update_prices(
+        validate=args.validate,
+        update_latest_prices=args.update_latest_prices,
+        update_5min_prices=args.update_5min_prices,
+        update_1hr_prices=args.update_1hr_prices,
+    )
 
 
 if __name__ == "__main__":
