aea87d2925a3f00253e2f7c7bc11af80254dc49b
Aria Corona <corona.aria.a@gmail.com>
2025-09-18 17:49:24 -0400

    Enable in-memory fallback

--- src/limiter.py
+++ src/limiter.py
@@ -5,4 +5,5 @@ limiter = Limiter(
     key_func=get_remote_address,
     default_limits=["50000 per day", "1000 per hour"],
     storage_uri="memcached://127.0.0.1:11221",
+    in_memory_fallback_enabled=True
 )
